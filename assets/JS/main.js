196d26b529636c5b6add4e83b33d916a2a65d34a
SimoneLollini <lollinisimone@gmail.com>
2022-10-05 16:01:20 +0200

    add prompt nome cognome e colore

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,4 +1,20 @@
+const inserisci = "inserisci il tuo"
+const inserisci_nome = (prompt(inserisci + " " + "nome"));
+const inserisci_cognome = (prompt(inserisci + " " + "cognome"));
+const inserisci_colore = (prompt(inserisci + " " + "colore preferito"));
+const currentYear_last = 22
 // Chiedi all’utente il suo nome
+// console.log("inserisci il tuo nome")
+
+
 // poi chiedi il suo cognome
+// console.log("inserisci il tuo cognome")
+
+
 // poi chiedi il suo colore preferito
+// console.log("inserisci il tuo colore preferito")
+
+
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+document.getElementById('input_name').innerHTML = inserisci_nome + inserisci_cognome + inserisci_colore + currentYear_last;
